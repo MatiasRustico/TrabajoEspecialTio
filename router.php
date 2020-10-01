7a14e8632c4ad5042696561dd839bab7ee43c3ab
Tompik04 <tompik14@gmail.com>
2020-10-01 19:57:19 -0300

    Creacion de games

--- router.php
+++ router.php
@@ -18,30 +18,9 @@ $params = explode('/', $action);
 
 // determina que camino seguir según la acción
 switch ($params[0]) {
-    case 'home':
-        showHome();
-        break;
-    case 'sumar':
-        sumar($params[1], $params[2]);
-        break;
-    case 'restar':
-        restar($params[1], $params[2]);
-        break;
-    case 'multiplicar':
-        multiplicar($params[1], $params[2]);
-        break;
-    case 'dividir':
-        dividir($params[1], $params[2]);
-        break;
-    case 'pi':
-        showPi();
-        break;
-    case 'about':
-        if (isset($params[1]))
-            showAbout($params[1]);
-        else
-            showAbout();
-        break;
+    case 'games':
+        $controller = new GamesController();
+        $controller->showGames();
     default:
         echo('404 Page not found');
         break;

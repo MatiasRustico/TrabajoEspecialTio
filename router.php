9f1254180e6161692091193635d70f29b95e4961
MatiasRustico <matytasrustgmail.com>
2020-10-01 19:57:11 -0300

    Creaccion de Market y Home

--- router.php
+++ router.php
@@ -1,8 +1,4 @@
 <?php
-
-include_once ('libreria/calculo.php');
-include_once ('sections.php');
-
 // defino la base url para la construccion de links con urls semánticas
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
@@ -21,26 +17,8 @@ switch ($params[0]) {
     case 'home':
         showHome();
         break;
-    case 'sumar':
-        sumar($params[1], $params[2]);
-        break;
-    case 'restar':
-        restar($params[1], $params[2]);
-        break;
-    case 'multiplicar':
-        multiplicar($params[1], $params[2]);
-        break;
-    case 'dividir':
-        dividir($params[1], $params[2]);
-        break;
-    case 'pi':
-        showPi();
-        break;
-    case 'about':
-        if (isset($params[1]))
-            showAbout($params[1]);
-        else
-            showAbout();
+    case 'market':
+        showMarket();
         break;
     default:
         echo('404 Page not found');
